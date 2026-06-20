ba4cb7ccaa7c42cf95dd90b5fd3217f7133c8783
Aryan0778 <saxenaaryan077@gmail.com>
2026-06-21 04:20:57 +0530

    Navbar and Sidebar Completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Layout from './components/Layout'
+import Dashboard from './pages/Dashboard'
+import { Route,Routes, useNavigate } from 'react-router-dom'
 
 const App = () => {
+const[user,setUser] = useState(null);
+const[token,setToken] = useState(null);
+const navigate = useNavigate();
+
+const clearAuth = ()=>{
+  try {
+    localStorage.removeItem("user")
+    localStorage.removeItem("token");
+    sessionStorage.removeItem("user");
+    sessionStorage.removeItem("token");
+  } catch (err) {
+    console.log("ClearAuth error",err);
+  }
+  setUser(null)
+  setToken(null)
+}
+
+const handleLogout = () =>{
+  clearAuth();
+  navigate("/login");
+}
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <>
+    <Layout/>
+    <Routes>
+    <Route path="/" element={<Dashboard/>}/>
+    </Routes>
+    </>
   )
 }
 
---
+++ Frontend/src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import {styles} from '../assets/dummyStyles'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+
+const Layout = ({onLogout,user}) => {
+    const [sidebarCollapsed,setSideBarCollapsed] = useState(false);
+  return (
+    <div className={styles.layout.root}>
+        <Navbar user={user} onLogout={onLogout}/>
+        <Sidebar user={user} isCollapsed={sidebarCollapsed} setIsCollapsed={setSideBarCollapsed}/>
+    </div>
+  )
+}
+
+export default Layout
